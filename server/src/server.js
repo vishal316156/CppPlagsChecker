ad418b9091f0bf64c285bb53785d70ecb4b1e6de
Marcox-try083 <vishalnitjsr2605ee@gmail.com>
2026-07-10 21:14:53 +0530

    chore: configure backend for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { checkCppSimilarity } from "./core/checkCppSimilarity.js";
+import cors from "cors";
 
+app.use(cors());
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
     express.json({
@@ -47,5 +49,5 @@ app.post("/compare", (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Comparison server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });

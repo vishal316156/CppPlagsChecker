2f58cf413b095c0f197e9b70f8c2529e5439c8b7
Marcox-try083 <vishalnitjsr2605ee@gmail.com>
2026-07-10 21:26:05 +0530

    configure frontend and backend for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,20 @@
 import express from "express";
-
-import { checkCppSimilarity } from "./core/checkCppSimilarity.js";
 import cors from "cors";
 
-app.use(cors());
+import { checkCppSimilarity } from "./core/checkCppSimilarity.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(
     express.json({
         limit: "1mb"
     })
 );
+
 
 
 app.post("/compare", (req, res) => {
